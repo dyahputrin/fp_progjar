17148747892344b8a3142b3952b8e75f69c4f215
Dyah Putri <putri.nariswari@gmail.com>
2022-06-14 17:01:26 +0700

    game commit

--- progjar9/server.py
+++ progjar9/server.py
@@ -1,60 +1,76 @@
-from socket import *
 import socket
-import threading
-import logging
-import time
-import sys
+from _thread import *
+import pickle
+from game import Game
+
+server = "192.168.0.133"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen()
+print("Waiting for a connection, Server Started")
+
+connected = set()
+games = {}
+idCount = 0
 
 
-from  protocol import PlayerServerProtocol
-fp = PlayerServerProtocol()
+def threaded_client(conn, p, gameId):
+    global idCount
+    conn.send(str.encode(str(p)))
 
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(4096).decode()
 
-class ProcessTheClient(threading.Thread):
-    def __init__(self, connection, address):
-        self.connection = connection
-        self.address = address
-        threading.Thread.__init__(self)
+            if gameId in games:
+                game = games[gameId]
 
-    def run(self):
-        while True:
-            data = self.connection.recv(32)
-            if data:
-                d = data.decode()
-                hasil = fp.proses_string(d)
-                hasil=hasil+"\r\n\r\n"
-                self.connection.sendall(hasil.encode())
+                if not data:
+                    break
+                else:
+                    if data == "reset":
+                        game.resetWent()
+                    elif data != "get":
+                        game.play(p, data)
+
+                    conn.sendall(pickle.dumps(game))
             else:
                 break
-        self.connection.close()
+        except:
+            break
+
+    print("Lost connection")
+    try:
+        del games[gameId]
+        print("Closing Game", gameId)
+    except:
+        pass
+    idCount -= 1
+    conn.close()
 
 
-class Server(threading.Thread):
-    def __init__(self,ipaddress='0.0.0.0',port=8889):
-        self.ipinfo=(ipaddress,port)
-        self.the_clients = []
-        self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        threading.Thread.__init__(self)
 
-    def run(self):
-        logging.warning(f"server berjalan di ip address {self.ipinfo}")
-        self.my_socket.bind(self.ipinfo)
-        self.my_socket.listen(1)
-        while True:
-            self.connection, self.client_address = self.my_socket.accept()
-            logging.warning(f"connection from {self.client_address}")
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
 
-            clt = ProcessTheClient(self.connection, self.client_address)
-            clt.start()
-            self.the_clients.append(clt)
+    idCount += 1
+    p = 0
+    gameId = (idCount - 1)//2
+    if idCount % 2 == 1:
+        games[gameId] = Game(gameId)
+        print("Creating a new game...")
+    else:
+        games[gameId].ready = True
+        p = 1
 
 
-def main():
-    svr = Server(ipaddress='0.0.0.0',port=6666)
-    svr.start()
-
-
-if __name__ == "__main__":
-    main()
-
+    start_new_thread(threaded_client, (conn, p, gameId))
