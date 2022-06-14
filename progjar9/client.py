3d91885fe436b0bf77f5544b2af366d3bf36aae8
bagusistighfar86 <bagusistighfar86@gmail.com>
2022-06-14 21:11:08 +0700

    Change Client

--- progjar9/client.py
+++ progjar9/client.py
@@ -1,29 +1,43 @@
 import pygame
 from network import Network
 import pickle
+
+from pygame import mixer
+pygame.mixer.init()
+mixer.music.load('./music/background.wav')
+mixer.music.play(-1)
+
+
 pygame.font.init()
 
-width = 700
-height = 700
+width = 1920
+height = 1080
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
 
 class Button:
-    def __init__(self, text, x, y, color):
+    def __init__(self, text, x, y):
         self.text = text
         self.x = x
         self.y = y
-        self.color = color
-        self.width = 150
-        self.height = 100
+        self.width = 160
+        self.height = 165
 
 
     def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-        font = pygame.font.SysFont("comicsans", 40)
-        text = font.render(self.text, 1, (255,255,255))
-        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
+        font = pygame.font.Font("AznKnuckles.otf", 40)
+        if self.text == "Rock":
+            btn = pygame.image.load("./img/rock.png")
+            win.blit(btn, (self.x + round(self.width/2) - round(btn.get_width()/2), self.y + round(self.height/2) - round(btn.get_height()/2)))
+        elif self.text == "Scissors":
+            btn = pygame.image.load("./img/scissors.png")
+            win.blit(btn, (self.x + round(self.width/2) - round(btn.get_width()/2), self.y + round(self.height/2) - round(btn.get_height()/2)))
+        elif self.text == "Paper":
+            btn = pygame.image.load("./img/paper.png")
+            win.blit(btn, (self.x + round(self.width/2) - round(btn.get_width()/2), self.y + round(self.height/2) - round(btn.get_height()/2)))
+        text = font.render(self.text, 1, (0,0,0))
+        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) + round(btn.get_height()/2) + 20))
 
     def click(self, pos):
         x1 = pos[0]
@@ -35,19 +49,17 @@ class Button:
 
 
 def redrawWindow(win, game, p):
-    win.fill((128,128,128))
+    bg = pygame.image.load("./img/background.jpg")
+    win.blit(bg, (0, 0))
 
     if not(game.connected()):
-        font = pygame.font.SysFont("comicsans", 80)
-        text = font.render("Waiting for Player...", 1, (255,0,0), True)
+        font = pygame.font.Font("AznKnuckles.otf", 80)
+        text = font.render("Waiting for Player...", 1, (70, 70, 70))
         win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
     else:
-        font = pygame.font.SysFont("comicsans", 60)
-        text = font.render("Your Move", 1, (0, 255,255))
-        win.blit(text, (80, 200))
-
-        text = font.render("Opponents", 1, (0, 255, 255))
-        win.blit(text, (380, 200))
+        font = pygame.font.Font("AznKnuckles.otf", 60)
+        text = font.render("Your Move Opponents", 1, (0, 51,102))
+        win.blit(text, (width/2 - text.get_width()/2, height/2 - 300))
 
         move1 = game.get_player_move(0)
         move2 = game.get_player_move(1)
@@ -70,11 +82,11 @@ def redrawWindow(win, game, p):
                 text2 = font.render("Waiting...", 1, (0, 0, 0))
 
         if p == 1:
-            win.blit(text2, (100, 350))
-            win.blit(text1, (400, 350))
+            win.blit(text2, (width / 2 - text.get_width() / 2, height / 2 - text.get_height() / 2))
+            win.blit(text1, (width / 2 - text.get_width() / 2 + 400, height / 2 - text.get_height() / 2))
         else:
-            win.blit(text1, (100, 350))
-            win.blit(text2, (400, 350))
+            win.blit(text2, (width / 2 - text.get_width() / 2, height / 2 - text.get_height() / 2))
+            win.blit(text1, (width / 2 - text.get_width() / 2 + 400, height / 2 - text.get_height() / 2))
 
         for btn in btns:
             btn.draw(win)
@@ -82,7 +94,7 @@ def redrawWindow(win, game, p):
     pygame.display.update()
 
 
-btns = [Button("Rock", 50, 500, (0,0,0)), Button("Scissors", 250, 500, (255,0,0)), Button("Paper", 450, 500, (0,255,0))]
+btns = [Button("Rock", 680, 700), Button("Scissors", 880, 700), Button("Paper", 1080, 700)]
 def main():
     run = True
     clock = pygame.time.Clock()
@@ -109,7 +121,7 @@ def main():
                 print("Couldn't get game")
                 break
 
-            font = pygame.font.SysFont("comicsans", 90)
+            font = pygame.font.Font("AznKnuckles.otf", 90)
             if (game.winner() == 1 and player == 1) or (game.winner() == 0 and player == 0):
                 text = font.render("You Won!", 1, (255,0,0))
             elif game.winner() == -1:
@@ -117,7 +129,7 @@ def main():
             else:
                 text = font.render("You Lost...", 1, (255,0,0))
 
-            win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
+            win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2 - 130))
             pygame.display.update()
             pygame.time.delay(2000)
 
@@ -145,10 +157,11 @@ def menu_screen():
 
     while run:
         clock.tick(60)
-        win.fill((128, 128, 128))
-        font = pygame.font.SysFont("comicsans", 60)
-        text = font.render("Click to Play!", 1, (255,0,0))
-        win.blit(text, (100,200))
+        bg = pygame.image.load("./img/background.jpg")
+        win.blit(bg, (0, 0))
+        font = pygame.font.Font("AznKnuckles.otf", 150)
+        text = font.render("Click to Play!", 1, (70,70,70))
+        win.blit(text, (width / 2 - text.get_width() / 2, height / 2 - text.get_height() / 2))
         pygame.display.update()
 
         for event in pygame.event.get():
